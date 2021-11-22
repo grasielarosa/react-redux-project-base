6d92c53093dc1e3a4874660726d23fcd7b74ddac
grasielarosa <grasielarosaa@gmail.com>
2021-11-22 10:34:04 +0100

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['prettier', 'react', 'react-hooks', '@babel'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': 0,
+    'import/prefer-default-export': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
---
+++ src/App.js
@@ -0,0 +1,8 @@
+/* eslint-disable react/function-component-definition */
+import React from 'react';
+
+function App() {
+  return <h1>Hello, World!</h1>;
+}
+
+export default App;
