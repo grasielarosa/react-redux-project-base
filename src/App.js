91428014d932c52b77f81b627171f01e41e46cb7
grasielarosa <grasielarosaa@gmail.com>
2021-11-24 10:16:24 +0100

    private route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 /* eslint-disable react/function-component-definition */
 import React from 'react';
+import { Router } from 'react-router-dom';
 
-import AppWrapper from './routes';
+import history from './services/history';
 import GlobalStyle from './styles/GlobalStyles';
+import Header from './components/Header';
+import Routes from './routes';
 
 function App() {
   return (
-    <>
-      <AppWrapper />
+    <Router history={history}>
+      <Header />
+      <Routes />
       <GlobalStyle />
-    </>
+    </Router>
   );
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,16 @@
 /* eslint-disable react/function-component-definition */
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
-import history from '../services/history';
-import Header from '../components/Header';
+import MyRoute from './MyRoute';
 import Login from '../pages/Login';
 import Page404 from '../pages/Page404';
 
-export default function AppWrapper() {
+export default function Routes() {
   return (
-    <Router history={history}>
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<Login />} />
-        <Route path="*" element={<Page404 />} />
-      </Routes>
-    </Router>
+    <Switch>
+      <MyRoute exact path="/" component={Login} isClosed />
+      <MyRoute path="*" component={Page404} />
+    </Switch>
   );
 }
