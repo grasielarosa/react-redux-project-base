90d67a9c4cdb9c9de02147be52c3f7b98561c4f5
grasielarosa <grasielarosaa@gmail.com>
2021-11-25 12:05:26 +0100

    update readme and axios config

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL:
-    'https://to-do-app-grasiela-rosa-default-rtdb.europe-west1.firebasedatabase.app/',
+    'YOUR_URL_HERE',
 });
